0177b9bbf84921cccd532a6069fb897206ad2392
Martin Hutchens <mhutchens@captechconsulting.com>
2020-04-20 16:32:43 -0400

    init drawing/animation of line; adding turfJs for calculation of distance between points

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -9,18 +9,51 @@ import { Map } from 'mapbox-gl';
 })
 export class MapComponent implements OnInit {
   /** CapTech Offices */
-  readonly captechOffices = {
-    clt: [-80.845750, 35.225759],
+  readonly captechOffices: CapTechOfficeCord = {
     atl: [-84.386015, 33.771060],
+    clt: [-80.845750, 35.225759],
     rva: [-77.527264, 37.605657],
     dc: [-77.350846, 38.952584],
-    chi: [-87.636008, 41.879860],
     phi: [-75.413489, 40.079056],
     cmh: [-82.997026, 39.959019], // columbus, oh
+    chi: [-87.636008, 41.879860],
     den: [-104.990875, 39.749686]
   };
 
   map: Map;
+
+  drawLine;
+
+  data = {
+    type: 'FeatureCollection',
+    features: [
+      {
+        type: 'Feature',
+        properties: {},
+        geometry: {
+          type: 'LineString',
+          coordinates: [
+            this.captechOffices.den,
+            this.captechOffices.atl,
+            this.captechOffices.clt,
+            this.captechOffices.rva,
+            this.captechOffices.dc,
+            this.captechOffices.phi,
+            this.captechOffices.cmh,
+            this.captechOffices.chi,
+            this.captechOffices.den
+          ]
+        }
+      }
+    ]
+  };
+
+  paint = {
+    'line-color': '#888',
+    'line-width': 4
+  };
+
+  private timer: number;
 
   constructor() {
   }
@@ -31,14 +64,39 @@ export class MapComponent implements OnInit {
   setMap(event): void {
     this.map = event;
 
-    this.map.fitBounds(new mapboxgl.LngLatBounds([this.captechOffices.den[0], this.captechOffices.den[1]], [this.captechOffices.phi[0], this.captechOffices.phi[1]]), {padding: 50});
+    this.map.fitBounds(new mapboxgl.LngLatBounds(this.captechOffices.den, this.captechOffices.phi), {padding: 50});
 
     Object.keys(this.captechOffices).forEach(key => {
       const marker = document.createElement('div');
       marker.className = 'marker';
       marker.id = key;
-      new mapboxgl.Marker(marker).setLngLat([this.captechOffices[key][0], this.captechOffices[key][1]]).addTo(this.map);
+      new mapboxgl.Marker(marker).setLngLat(this.captechOffices[key]).addTo(this.map);
     });
-  }
 
+    const temp = this.data;
+    const coordinates = temp.features[0].geometry.coordinates;
+    temp.features[0].geometry.coordinates = [coordinates[0]];
+    this.drawLine = temp;
+    let i = 0;
+    this.timer = window.setInterval(() => {
+      if (i < coordinates.length) {
+        temp.features[0].geometry.coordinates.push(coordinates[i]);
+        this.drawLine = {...temp};
+        i++;
+      } else {
+        window.clearInterval(this.timer);
+      }
+    }, 300);
+  }
+}
+
+export interface CapTechOfficeCord {
+  clt: [number, number];
+  atl: [number, number];
+  rva: [number, number];
+  dc: [number, number];
+  chi: [number, number];
+  phi: [number, number];
+  cmh: [number, number];
+  den: [number, number];
 }
