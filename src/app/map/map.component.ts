e695a5123d7790f3f2d4b2938e04ba5cb354fef9
Martin Hutchens <mhutchens@captechconsulting.com>
2020-04-27 15:24:56 -0400

    commenting of code

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 import { LineLayout, LinePaint, Map, SymbolLayout } from 'mapbox-gl';
 import * as turf from '@turf/turf';
@@ -10,17 +10,19 @@ import { CapTechOfficeCoord } from '../models/capTechOfficeCoord';
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
-export class MapComponent implements OnInit {
+export class MapComponent {
+  /**
+   * Number of steps to use in the arc and animation, more steps means
+   * a smoother arc and animation, but too many steps will result in a
+   * low frame rate
+   */
   readonly frameRate = 500;
-
-  /** CapTech Offices */
-  captechOffices: CapTechOfficeCoord = new CapTechOfficeCoord();
 
   map: Map; // mapbox-gl reference
   line;
   point;
 
-  // styling for progress line drawn, used in html
+  // styling for completed route line, used in html
   linePaint: LinePaint = {
     'line-color': '#888',
     'line-width': 4
@@ -29,6 +31,8 @@ export class MapComponent implements OnInit {
     'line-cap': 'round',
     'line-join': 'bevel'
   };
+
+  // styling for the completed route lead point, used in html
   pointLayout: SymbolLayout = {
     'icon-image': 'fire-station-15',
     'icon-rotate': ['get', 'bearing'],
@@ -37,32 +41,32 @@ export class MapComponent implements OnInit {
     'icon-ignore-placement': true
   };
 
-  /**
-   * Number of steps to use in the arc and animation, more steps means
-   * a smoother arc and animation, but too many steps will result in a
-   * low frame rate
-   */
+  /** calculated steps needed to complete the drawing of the route. */
   private steps = 0;
   /** Used to track the current frame when drawing the route on map. */
   private counter = 0;
+  /** coordinates to be drawn during animation of line/point on map. */
+  private drawnRouteCoords = [];
 
+  /** CapTech Offices */
+  captechOffices: CapTechOfficeCoord = new CapTechOfficeCoord();
   /** coordinates calculated from total distance along the selected route line */
-  private lineData;
-  private calculatedCoords = [];
+  private derivedRouteCoords = [];
 
   constructor(private activityService: ActivityService) {
   }
 
-  ngOnInit(): void {
-  }
-
-  setMap(event): void {
-    this.map = event;
+  /**
+   * Sets up MapBox after it loads
+   * @param mapRef reference to the MapBox element
+   */
+  setMap(mapRef): void {
+    this.map = mapRef;
 
     this.map.fitBounds(new mapboxgl.LngLatBounds(this.captechOffices.den, this.captechOffices.phi), {padding: 50});
 
     this.drawOfficeMarkers();
-    this.findDistance();
+    this.findTraveledCoordinates();
   }
 
   private drawOfficeMarkers(): void {
@@ -74,7 +78,11 @@ export class MapComponent implements OnInit {
     });
   }
 
-  private findDistance(type = 'totalMiles') {
+  /**
+   * Calculates coordinates for number of completed miles then calls drawCompletedRoute to draw on map.
+   * @param type what type of miles to build completed route off of
+   */
+  private findTraveledCoordinates(type = 'totalMiles') {
     // provides the ability to change the starting point of the route to the selected office
     // todo add logic to allow for a selected 'starting' office
     const selectedRoute = this.captechOffices.getDefaultRoute();
@@ -87,12 +95,17 @@ export class MapComponent implements OnInit {
       let loopsMade = 0; // tracks how many loops around selectedRoute
       let partialLoopSteps = 0; // tracks number of steps scaled down to remainder distance
 
+      // Loop through the route to account for a total milesTraveled
+      // being longer then the selectedRoute's total distance.
       while (milesTraveled >= 0) {
+        // if milesTraveled is longer then the selectedRoute's perimeter add all coordinates along the selected route
         if (milesTraveled > totalOfficePerimeter) {
           for (let i = 0; i < totalOfficePerimeter; i += totalOfficePerimeter / this.frameRate) {
             derivedCoords.push(turf.along(selectedRoute, i, {units: 'miles'}).geometry.coordinates);
           }
           loopsMade++;
+
+          // if milesTraveled is less then the selectedRoute's perimeter add only the coordinates for the traveled part
         } else {
           partialLoopSteps = Math.ceil(milesTraveled / Math.ceil(totalOfficePerimeter / this.frameRate));
           for (let i = 0; i < milesTraveled; i += milesTraveled / partialLoopSteps) {
@@ -103,13 +116,18 @@ export class MapComponent implements OnInit {
         milesTraveled -= totalOfficePerimeter;
       }
 
-      this.lineData = derivedCoords;
+      this.derivedRouteCoords = derivedCoords;
       this.steps = partialLoopSteps + (this.frameRate * loopsMade);
+
       this.drawCompletedRoute(startOfRoute);
     });
   }
 
-  private drawCompletedRoute(startOfRoute): void {
+  /**
+   * Sets inital states of the line and point features on map then calls {@link myAnimator}
+   * @param startOfRoute the starting place for the line and point
+   */
+  private drawCompletedRoute(startOfRoute: [number, number]): void {
     // setup initial states for the point and line
     this.point = {
       type: 'FeatureCollection',
@@ -142,11 +160,11 @@ export class MapComponent implements OnInit {
 
   /**
    * Animates the drawing of both the line and point onto the map
-   * by iterating through the coordinates stored in {@link calculatedCoords}.
+   * by iterating through the coordinates stored in {@link derivedRouteCoords}.
    */
   private myAnimator() {
     // append new coordinates to the lineString
-    this.calculatedCoords.push(this.lineData[this.counter]);
+    this.drawnRouteCoords.push(this.derivedRouteCoords[this.counter]);
 
     // then update the map
     this.line = {
@@ -157,7 +175,7 @@ export class MapComponent implements OnInit {
           properties: {},
           geometry: {
             type: 'LineString',
-            coordinates: this.calculatedCoords
+            coordinates: this.drawnRouteCoords
           }
         }]
     };
@@ -171,7 +189,7 @@ export class MapComponent implements OnInit {
           properties: {},
           geometry: {
             type: 'Point',
-            coordinates: this.calculatedCoords[this.counter]
+            coordinates: this.drawnRouteCoords[this.counter]
           }
         }
       ]
